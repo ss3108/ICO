294a1748246543de801a3807383d88e0e2140902
Shruti Singh <shrutisingh3108@gmail.com>
2022-11-15 01:32:58 +0530

    hardhat goreli config

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+//Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x85f1fda3E6f093B5c12642193962660F1b732C2b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
